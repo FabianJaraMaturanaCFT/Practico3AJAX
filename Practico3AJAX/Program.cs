95ca645046ac599afa0eaa318746100a7500c900
Fabian <fajara23@cftsa.cl>
2024-10-22 01:59:20 -0300

    Implementacion de la Libreria Microsoft.AspNetCore.Localization (en a es)

--- Practico3AJAX/Program.cs
+++ Practico3AJAX/Program.cs
@@ -1,23 +1,45 @@
 using Microsoft.EntityFrameworkCore;
 using Practico3AJAX.Data;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Añadir localización (carpeta Resources para los archivos .resx)
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
 builder.Services.AddDbContext<ProyectoDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// Añadir soporte para la localización en las vistas
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization()
+    .AddDataAnnotationsLocalization();
+
 var app = builder.Build();
+
+// Definir las culturas soportadas (en este caso, inglés y español)
+var supportedCultures = new[] { new CultureInfo("en"), new CultureInfo("es") };
+
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("es"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
+
+// Aplicar la configuración de localización
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -28,8 +50,10 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Definir las rutas
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
