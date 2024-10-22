c244374e532ec83ebc7e12dd2e0bbc69ee1cca1b
Fabian <fajara23@cftsa.cl>
2024-10-22 00:17:45 -0300

    Eliminé la base de datos 'Practico3' y creé una nueva llamada 'Practico3AJAX'. Ademas realicé las migraciones

--- Practico3AJAX/Program.cs
+++ Practico3AJAX/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=AsignacionHerramientas}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
