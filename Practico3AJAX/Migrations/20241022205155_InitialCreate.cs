fecc2fbdfc9d97d248ce7d433f9d486709392163
Damian Alexander Jelvez Soto <128104620+Damian211997@users.noreply.github.com>
2024-10-22 23:34:32 -0300

    Index Actualizados

---
+++ Practico3AJAX/Migrations/20241022205155_InitialCreate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Practico3AJAX.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
